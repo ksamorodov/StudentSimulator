f77f5893a244ef67cf96329695985af427b8d8c9
Bratko73 <alexlapt73@gmail.com>
2020-08-13 20:27:22 +0300

    add Fullscreen and Vertical orientation

--- app/src/main/java/stankin/project/studentsimulator/MainActivity.kt
+++ app/src/main/java/stankin/project/studentsimulator/MainActivity.kt
@@ -1,11 +1,16 @@
 package stankin.project.studentsimulator
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // delete status string
+        var w: Window = getWindow()
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
